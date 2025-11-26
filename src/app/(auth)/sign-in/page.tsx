1f9cb2db56be5d419a09708a9ffec5b64c8b4036
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2025-11-26 15:35:26 +0330

    Add Inter Font , Add Auth Components(Signin,Signup)

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { SignInCard } from '@/features/auth/components/SignInCard'
+
+
+const page = () => {
+  return (
+    <SignInCard/>
+  )
+}
+
+export default page
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { SignUpCard } from '@/features/auth/components/SignUpCard';
+import React from 'react'
+
+const page = () => {
+  return (
+    <SignUpCard/>
+  )
+}
+
+export default page
