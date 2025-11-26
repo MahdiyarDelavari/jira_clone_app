d7f949e6f590c89f82c2a430070d3d1ab4eeb4de
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2025-11-26 10:35:22 +0330

    Initial The App

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant={"destructive"} size={"sm"}>Shadcn Working</Button>
+      <p className="text-orange-700">Tailwind Working</p>
+    </div>
+
+  );
+}
