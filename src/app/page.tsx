a346d4168f778f14e3be5ec76495352717fa663b
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2025-11-28 17:25:50 +0330

    Building A Session Middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+	const router = useRouter();
+  const { data, isLoading } = useCurrent(); 
+  const {mutate} = useLogout()
+
+	useEffect(() => {
+		if (!data && !isLoading) {
+			router.push("/sign-in");
+		}
+  }, [data]);
+  
+
   return (
     <div className="flex gap-4">
-      <Button variant={"primary"}>Primary</Button>
-      <Button variant={"secondary"}>Secondary</Button>
-      <Button variant={"destructive"}>Destructive</Button>
-      <Button variant={"ghost"}>Ghost</Button>
-      <Button variant={"muted"}>Muted</Button>
-      <Button variant={"outline"}>Outline</Button>
-      <Button variant={"teritary"}>Teritary</Button>
-      
+      Only visile To You AuthorizeUser
+      <button onClick={()=>mutate()}>logout</button>
     </div>
-
   );
 }
