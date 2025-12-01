dcc2ff4dd8fcef7cc7b989f24f84ec3735abb8ac
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2025-12-01 12:22:47 +0330

    Create Workspace-Members

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,18 +1,37 @@
-import { workspaces } from '@/features/workspaces/server/route';
+
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middlewate";
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
-import { ID } from "node-appwrite";
+import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
+import { MemberRole } from "@/features/members/types";
+import { generateInviteCode } from "@/lib/utils";
 
 const app = new Hono()
 	.get("/", sessionMiddleware, async (c) => { 
+		const user = c.get("user");
 		const db = c.get("databases");
+
+		const members = await db.listDocuments(
+			DATABASE_ID,
+			MEMBERS_ID,
+			[Query.equal("userId", [user.$id])]
+		)
+		if(members.total === 0){
+			return c.json({ data: {documents:[],total:0} });
+		}
+
+		const workspaceIds = members.documents.map((member) => member.workspaceId);
+
 
 		const workspaces = await db.listDocuments(
 			DATABASE_ID,
 			WORKSPACES_ID,
+			[
+				Query.orderDesc("$createdAt"),
+				Query.contains("\$id", workspaceIds)
+			]
 		)
 
 		return c.json({ data: workspaces });
@@ -54,8 +73,20 @@ const app = new Hono()
 				name,
 				userId: user.$id,
 				imageUrl: uploadedImageUrl,
+				inviteCode:generateInviteCode()
 			}
 		);
+
+		await db.createDocument(
+			DATABASE_ID,
+			MEMBERS_ID,
+			ID.unique(),
+			{
+				userId: user.$id,
+				workspaceId: workspace.$id,
+				role: MemberRole.ADMIN,
+			}
+		)
 
 		return c.json({ data: workspace });
 	}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function generateInviteCode() { 
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  const length = Math.floor(Math.random() * 4) + 6; // Generates a length between 6 and 9
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+  
+}
