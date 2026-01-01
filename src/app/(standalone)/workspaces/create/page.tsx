000ec2e80b5a4a54f0ed519e4fc0b5c71bf64dba
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2026-01-01 17:42:55 +0330

    Implement Standalone-Layout

---
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -0,0 +1,17 @@
+import { getCurrent } from '@/features/auth/actions';
+import { CreateWorkspaceForm } from '@/features/workspaces/components/CreateWorkspaceForm'
+import { redirect } from 'next/navigation';
+import React from 'react'
+
+const page = async () => {
+  const user = await getCurrent();
+  if (!user) redirect('/sign-in');
+  
+  return (
+    <div className='w-full lg:max-w-xl'>
+      <CreateWorkspaceForm/>
+    </div>
+  )
+}
+
+export default page
